17c6e0dc3c4d74bdfe6ce85f3324459f9485f43f
Ian Quick <ian.quick@gmail.com>
2014-11-09 10:06:26 -0500

    ghetto deploy

---
+++ src/albatross/deploy.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+set -e
+
+HOST="ian@192.168.1.5"
+RELEASE=`date "+%Y%m%d%H%M%S"`
+DIST_DIR="/usr/local/albatross"
+RELEASE_DIR="$DIST_DIR/$RELEASE"
+
+JAR="$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')"
+
+ssh $HOST "mkdir -p $RELEASE_DIR"
+scp target/albatross-standalone.jar "$HOST:$RELEASE_DIR"
+
+ssh $HOST "ln -sf $RELEASE_DIR/albatross-standalone.jar current"
+
+ssh $HOST "sudo service albatross restart"
